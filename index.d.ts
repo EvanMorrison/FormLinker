74d73c07b66806aac77d4aa0b0ff002a3d71f65a
Evan Morrison <evan.morrison@tazworks.com>
2020-04-26 12:32:30 -0600

    add comments to d.ts file; eslint ignore dist folder

--- index.d.ts
+++ index.d.ts
@@ -19,28 +19,46 @@ declare module "form-linker" {
     originalData: {};
     errors: {};
     changeCallback(): void;
+    /** Used internally by FormLinker */
     calcFields(schema?: {}, prefix?: string, fields?: string[]): string[];
+    /** Used internally by FormLinker */
     convert(fieldName: string, value: any): any;
+    /** Returns current errors for a given fieldName */
     getError(fieldName: string): string[];
+    /** Returns all current errors */
     getErrors(): {};
-    setError(fieldName: string, errors: {}, triggerCallback?: boolean): void;
-    setErrors(errors: any, triggerCallback?: boolean): void;
+    /** Set the errors for specific field */
+    setError(fieldName: string, errors: string[], triggerCallback?: boolean): void;
+    /** Set errors for all fields with matching property name or path in the errors parameter */
+    setErrors(errors: {}, triggerCallback?: boolean): void;
+    /** Returns the value for a given fieldName */
     getValue(fieldName: string): any;
+    /** Returns the current data property */
     getValues(): {};
+    /** Set the value for a given fieldName. Sets the "masked" value in data and the "formatted" value in thise.parsedData values */
     setValue(fieldName: string, value: any, triggerCallback?: boolean): void;
-    setValues(values: any, triggerCallback?: boolean): void;
+    /** Set the values for all fields with matching property name or path in the values parameter. Runs setValue for each field. */
+    setValues(values: {}, triggerCallback?: boolean): void;
+    /** Like setValues, except instead of running setValue for each field, it runs it through the converters sets the result in this.data. It does not set or change this.parsedData values. */
     setValuesFromParsed(values: any): void;
+    /** Used internally by FormLinker. Runs the value through the formatter for the applicable schema type. */
     format(fieldName: string, value: any): {
-      errors: any[];
+      errors: string[];
       formatted: any;
       parsed: any;
       valid: boolean;
     };
+    /** Used internally by FormLinker */
     mask(fieldName: string, value: any): any;
+    /** Runs this.format on each field until it finds an error. Returns true if no error is found. */
     isValid(): boolean;
+    /** Validates the given field. Internally runs this.format on the given field, then updates the field's errors, data value and parsedData value */
     validate(fieldName: string, triggerCallback?: boolean): void;
+    /** Runs this.validate for every field */
     validateAll(triggerCallback?: boolean): void;
+    /** Returns an object with fieldNames and values that are different thn "original" parameter. If there are no differences returns an empty object */
     extractDifferences(original: any): {};
+    /** Updates the schema for the current FormLinker instance - used if the fieldNames change or are set dynamically */
     updateSchema(schema: {}): void;
   }
 }
